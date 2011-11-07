5c0a6208a1dbe9b70b3aecabe4d7e589aa711fb2
Sylvain Fay-Chatelard <sylvain.faychatelard@gmail.com>
2011-11-07 20:49:01 +0100

    - add another ellipse - redraw all terrain

--- src/fr/umlv/yourobot/Main.java
+++ src/fr/umlv/yourobot/Main.java
@@ -2,9 +2,8 @@ package fr.umlv.yourobot;
 
 import java.awt.Color;
 import java.awt.Graphics2D;
-import java.awt.RadialGradientPaint;
 import java.awt.geom.Ellipse2D;
-import java.util.Random;
+import java.awt.geom.Rectangle2D;
 
 import fr.umlv.zen.Application;
 import fr.umlv.zen.ApplicationCode;
@@ -18,17 +17,11 @@ public class Main {
 		final int HEIGHT = 600;
 		final int SIZE = 30;
 
-		Application.run("Keyboard", WIDTH, HEIGHT, new ApplicationCode() {
+		Application.run("You robot", WIDTH, HEIGHT, new ApplicationCode() {
 			@Override
 			public void run(final ApplicationContext context) {
-				final Random random = new Random(0);
-
-				float x=10;
-				float y=10;
-				x = random.nextInt(WIDTH);
-				y = random.nextInt(HEIGHT);
-
-				final Ellipse2D ellipse = new Ellipse2D.Float(x - SIZE/2, y - SIZE/2, SIZE, SIZE);
+				final Ellipse2D ellipse = new Ellipse2D.Float(100 - SIZE/2, 100 - SIZE/2, SIZE, SIZE);
+				final Ellipse2D ellipse2 = new Ellipse2D.Float(100 - SIZE/2, 100 - SIZE/2, SIZE, SIZE);
 				for(;;) {
 					final KeyboardEvent event = context.waitKeyboard();
 					if (event == null) {
@@ -39,9 +32,12 @@ public class Main {
 						public void render(Graphics2D graphics) {
 							double x = ellipse.getX();
 							double y = ellipse.getY();
-							graphics.setBackground(Color.WHITE);
-							graphics.setColor(Color.WHITE);
-							graphics.clearRect((int)x, (int)y, (int)ellipse.getWidth(), (int)ellipse.getHeight());
+							double x2 = ellipse2.getX();
+							double y2 = ellipse2.getY();
+							graphics.setBackground(new Color(238, 238, 238));
+							graphics.setColor(new Color(238, 238, 238));
+							Rectangle2D rectangle = new Rectangle2D.Float(0, 0, WIDTH, HEIGHT);
+							graphics.fill(rectangle);
 							switch(event.getKey()) {
 							case UP:
 								y-=10;
@@ -55,12 +51,25 @@ public class Main {
 							case RIGHT:
 								x+=10;
 								break;
+							case Z:
+								y2-=10;
+								break;
+							case S:
+								y2+=10;
+								break;
+							case Q:
+								x2-=10;
+								break;
+							case D:
+								x2+=10;
+								break;
 							default:
 							}
 							ellipse.setFrame(x, y, ellipse.getWidth(), ellipse.getHeight());
+							ellipse2.setFrame(x2, y2, ellipse.getWidth(), ellipse.getHeight());
 							graphics.setColor(Color.BLACK);
 							graphics.fill(ellipse);
-							System.out.println(event.toString());
+							graphics.fill(ellipse2);
 						}
 					});
 				}
